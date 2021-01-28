fdcb3c2e35232d1d53ba5d2168a0b2855ab6c746
Toasty <curtisgauntdev@gmail.com>
2021-01-27 23:59:07 -0500

    Prepared TOC. Preplanning structuring,

---
+++ Core.lua
@@ -0,0 +1,12 @@
+--functions i know ill need but dont want to  look up later
+--RandomRoll(min, max)
+--AddTradeMoney() won value?
+--SetTradeMoney(wonValue * 100 * 100)
+--GetPlayerTradeMoney() for verification (turn green when good?)
+
+-- saved leaderboard variables here
+local Leaderboard = {}
+
+-- bind events
+
+-- frame prefabs
